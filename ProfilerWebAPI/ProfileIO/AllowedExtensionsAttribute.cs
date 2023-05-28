5072f5885351d07c3c6d70121a18f58edcb66600
lkumbrella <L.Kumbrella@outlook.com>
2023-05-28 23:29:25 +0300

    File validation messages added

---
+++ ProfilerWebAPI/ProfileIO/AllowedExtensionsAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProfilerWebAPI.ProfileIO;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class AllowedExtensionsAttribute : ValidationAttribute
+{
+    private readonly string[] _extensions;
+
+    public AllowedExtensionsAttribute(string[] extensions)
+    {
+        _extensions = extensions;
+    }
+
+    protected override ValidationResult? IsValid(object? value,
+        ValidationContext validationContext)
+    {
+        if (value is not IFormFile file)
+        {
+            return new ValidationResult("File is required");
+        }
+
+        var extension = Path.GetExtension(file.FileName);
+
+        return _extensions.Contains(extension.ToLowerInvariant()) ?
+            ValidationResult.Success :
+            new ValidationResult("File is not supported");
+    }
+}
---
+++ ProfilerWebAPI/ProfileIO/FileSizeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProfilerWebAPI.ProfileIO;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class FileSizeAttribute : ValidationAttribute
+{
+    private readonly int _maxFileSize;
+
+    public FileSizeAttribute(int maxSize)
+    {
+        _maxFileSize = maxSize;
+    }
+
+    protected override ValidationResult? IsValid(object? value,
+        ValidationContext validationContext)
+    {
+        if (value is not IFormFile file)
+        {
+            return new ValidationResult("File is required");
+        }
+
+        return file.Length < _maxFileSize ?
+            ValidationResult.Success :
+            new ValidationResult("File is too big");
+    }
+}
